ba039587887b83f5a7c87965bc008e72fcdc738d
Bart Zuber <bzuber@neworbit.co.uk>
2021-11-14 21:39:09 +0100

    update demo

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,14 +1,19 @@
-import { useCronParser } from 'use-cron-parser'
+import { useCronParser } from "use-cron-parser";
+import { format } from "date-fns";
 import "./App.css";
 
 function App() {
-  const cronExpression = "10 * * * *";
+  const cronExpression = "10 0 * * *";
   const generator = useCronParser(cronExpression);
 
   return (
     <div className="App">
-      <div>{cronExpression}</div>
-      <div>{generator.next()}</div>
+      <div style={{ fontWeight: "bold" }}>Cron expression</div>
+      <div style={{ marginBottom: 30 }}>{cronExpression}</div>
+      <div>
+        Next occurrence scheduled at{" "}
+        {format(generator.next().value, "dd/MM/yyyy HH:mm:ss")}
+      </div>
     </div>
   );
 }
