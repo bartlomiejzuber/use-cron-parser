7177282a2f90bd9099db0d593963d72b36ef3ec2
Bart Zuber <bzuber@neworbit.co.uk>
2021-11-14 21:10:17 +0100

    add demo

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCronParser } from 'use-cron-parser'
+import "./App.css";
 
 function App() {
+  const cronExpression = "10 * * * *";
+  const generator = useCronParser(cronExpression);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{cronExpression}</div>
+      <div>{generator.next()}</div>
     </div>
   );
 }
